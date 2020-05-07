07e6ac0bcb12de3b0999db8e208201be65b6711c
Jessica <jessicamenius@gmail.com>
2020-05-06 21:26:55 -0500

    added time for 7

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 $(document).ready(function () {
   //   console.log("hello");
 
-  $("#btnSubmit1").on("click", function () {
+  $("#btnSubmit6").on("click", function () {
     var userText = $(textInput6).val();
-    localStorage.setItem("data", JSON.stringify({ task: userText }));
-
+    localStorage.setItem("data6", JSON.stringify({ task: userText }));
     render6Data();
   });
 
-  function render6Data() {
-    var item = JSON.parse(localStorage.getItem("data"));
+  $("#btnSubmit7").on("click", function () {
+    var userText = $(textInput7).val();
+    localStorage.setItem("data7", JSON.stringify({ task: userText }));
+    render7Data();
+  });
 
-    $("textInput6").val(item.data);
+  render6Data();
+  render7Data();
+
+  function render6Data() {
+    var item = JSON.parse(localStorage.getItem("data6"));
+    if (item != null) {
+      $("textInput6").val(item.task);
+    }
+  }
+
+  function render7Data() {
+    var item = JSON.parse(localStorage.getItem("data7"));
+    if (item != null) {
+      $("textInput7").val(item.task);
+    }
   }
 });
